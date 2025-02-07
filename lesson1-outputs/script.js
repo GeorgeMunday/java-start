9190d31d5f6f04defcdb88b42be391b70455bba4
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2025-02-07 09:35:24 +0000

    first 3 lessons

---
+++ lesson1-outputs/script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function() { //makes sure the document is full loaded 
+    document.getElementById("demo").innerText = "this is an output"; // adding the text
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("math").innerText = 5 + 6
+});
---
+++ lesson2-statments/script.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener("DOMContentLoaded", function() { //makes sure the document is full loaded 
+    let x,y,z
+    x = 1
+    y = 4
+    z = x + y
+    document.getElementById("demo").innerText = "the value of z = " + z + "."; })
